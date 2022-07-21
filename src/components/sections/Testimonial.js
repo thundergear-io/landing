13b9b48577aa8da913b0780d69641842b54b0e66
Chromico Rek <atech5122@gmail.com>
2022-07-21 19:03:35 +0400

    Update Testimonial.js

--- src/components/sections/Testimonial.js
+++ src/components/sections/Testimonial.js
@@ -44,8 +44,8 @@ const Testimonial = ({
   );
 
   const sectionHeader = {
-    title: 'Customer testimonials',
-    paragraph: 'Vitae aliquet nec ullamcorper sit amet risus nullam eget felis semper quis lectus nulla at volutpat diam ut venenatis tellus—in ornare.'
+    title: 'Some of our projects',
+    paragraph: 'Projects that we support and are maintained by us.'
   };
 
   return (
@@ -58,19 +58,19 @@ const Testimonial = ({
           <SectionHeader data={sectionHeader} className="center-content" />
           <div className={tilesClasses}>
 
-            <div className="tiles-item reveal-from-right" data-reveal-delay="200">
+          <div className="tiles-item reveal-from-right" data-reveal-delay="200">
+              <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
+                <span className="testimonial-item-name text-color-high">Roman Level</span>
+                <span className="text-color-low"> / </span>
+                <span className="testimonial-item-link">
+                  <a href="#0">MoStickers</a>
+                </span>
+              </div>
               <div className="tiles-item-inner">
                 <div className="testimonial-item-content">
                   <p className="text-sm mb-0">
                     — Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum cillum dolore eu fugiat.
                       </p>
-                </div>
-                <div className="testimonial-item-footer text-xs mt-32 mb-0 has-top-divider">
-                  <span className="testimonial-item-name text-color-high">Roman Level</span>
-                  <span className="text-color-low"> / </span>
-                  <span className="testimonial-item-link">
-                    <a href="#0">AppName</a>
-                  </span>
                 </div>
               </div>
             </div>
